d2705144a02496515a18c6721346391cc6a7941c
oui oui <adam.larkem@epitech.eu>
2025-07-05 21:31:21 +0200

    fix:(docker-compose)

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,10 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import dotenv from "dotenv";
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
