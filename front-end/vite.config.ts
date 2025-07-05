e431e6cfdd1fd748fe33092530b6e06dac8ab997
oui oui <adam.larkem@epitech.eu>
2025-07-05 21:23:49 +0200

    feat:(database) docker now start the databse with the front and the back

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,8 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+dotenv.config();
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    port: Number(process.env.FRONTEND_PORT),
+  },
 });
